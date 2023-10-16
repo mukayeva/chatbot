b29ad6dbd080f743c3c5c2d3a5f8e7f2f08c56f8
Dilara Mukayeva <dilmukay@WKMZT29BB044.global.publicisgroupe.net>
2023-10-16 15:37:07 +0100

    added the chatbot

---
+++ README.md
@@ -0,0 +1,9 @@
+# chatbot
+# install dependencies
+
+`npm install`
+`npm install @chatscope/chat-ui-kit-react`
+
+# start dev version on local
+
+`npm run dev`
---
+++ src/App.jsx
@@ -0,0 +1,123 @@
+import { useState } from 'react'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+import {dataset_part1, dataset_part2, initialText} from './data/dataset.jsx'
+
+const API_KEY = "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";
+
+const systemMessage = { 
+  "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
+}
+
+function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm AiChatbot! Ask me anything!",
+      sentTime: "just now",
+      sender: "ChatGPT"
+    }
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+
+
+  const handleSend = async (message) => {
+
+    const requirement = "Based on dataset Part 1 and Part 2, ";
+    const newMessage = {
+      message: requirement + message,
+      direction: 'outgoing',
+      sender: "user",
+      dontPrintMessage: false
+    };
+
+    const initialMessage = 
+    {
+      message: initialText + dataset_part1,
+      direction: 'outgoing',
+      sender: "user",
+      dontPrintMessage: true
+    };
+    
+    let newMessages = [...messages, initialMessage];
+    await processMessageToChatGPT(newMessages);
+
+    const secondMessage =    {
+      message: dataset_part2,
+      direction: 'outgoing',
+      sender: "user",
+      dontPrintMessage: true
+    };
+    
+    newMessages = [...newMessages, secondMessage];
+    await processMessageToChatGPT(newMessages);
+
+    newMessages = [...newMessages, newMessage];
+    setMessages(newMessages);
+    setIsTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message}
+    });
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,  
+        ...apiMessages
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", 
+    {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      setMessages([...chatMessages, {
+        message: data.choices[0].message.content,
+        sender: "ChatGPT"
+      }]);
+      setIsTyping(false);
+    });
+  }
+
+  return (
+    <div className="App">
+      <div style={{ position:"relative", height: "800px", width: "700px"  }}>
+        <MainContainer>
+          <ChatContainer>       
+            <MessageList 
+              scrollBehavior="smooth" 
+              typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
+            >
+              {messages.map((message, i) => {
+                if (!message.dontPrintMessage) {
+                  return <Message key={i} model={message} />
+                }
+              })}
+            </MessageList>
+            <MessageInput placeholder="Type message here" onSend={handleSend} />        
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
+  )
+}
+
+export default App
